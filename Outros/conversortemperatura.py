3034509f35efb6e3b907de6214672afcadc06f59
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-02 21:53:45 -0300

    Update conversortemperatura.py

--- Outros/conversortemperatura.py
+++ Outros/conversortemperatura.py
@@ -3,5 +3,5 @@
     converte para fahrenheit
 '''
 temperatura = float(input('Temperatura em graus Celsius: '))
-fahrenheint = (temperatura * 9 / 5) + 32
-print(f'A temperatura em Fahrenheit é {fahrenheint} °F')
+fahrenheit = (temperatura * 9 / 5) + 32
+print(f'A temperatura em Fahrenheit é {fahrenheit} °F')
