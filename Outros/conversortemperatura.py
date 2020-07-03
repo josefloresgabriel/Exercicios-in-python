ff571bc90f4eaa18a362e3dd1e1beefba138cc67
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-02 21:53:25 -0300

    Update conversortemperatura.py

--- Outros/conversortemperatura.py
+++ Outros/conversortemperatura.py
@@ -1,3 +1,7 @@
+'''
+    Programa que lê a temperatura em celsius e 
+    converte para fahrenheit
+'''
 temperatura = float(input('Temperatura em graus Celsius: '))
 fahrenheint = (temperatura * 9 / 5) + 32
 print(f'A temperatura em Fahrenheit é {fahrenheint} °F')
