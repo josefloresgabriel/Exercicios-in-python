346b2c78f1d70a844dba1bf70bc6c678c20745d5
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-02 22:00:41 -0300

    Update tabuada.py

--- Outros/tabuada.py
+++ Outros/tabuada.py
@@ -1,3 +1,7 @@
+'''
+    Programa que mostra a tabuada do numero escolhido
+    pelo usuario
+'''
 n = int(input('Informe o numero para saber a sua tabuada:  '))
 for c in range(0, 11):
     resp = n * c
