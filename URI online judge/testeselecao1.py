ff7fd9f9aff65bcf44972f42ef2fe6021931f481
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-17 17:29:55 -0300

    mais exercicios

---
+++ URI online judge/testeselecao1.py
@@ -0,0 +1,9 @@
+valores = str(input()).split()
+a = int(valores[0])
+b = int(valores[1])
+c = int(valores[2])
+d = int(valores[3])
+if b > c and d > a and c + d > a + b and a % 2 == 0 and c > 0 and d > 0:
+    print('Valores aceitos')
+else:
+    print('Valores nao aceitos')
