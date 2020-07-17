7473965b5ad705e630f9eea04df199c8d56cf4c1
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-16 22:01:51 -0300

    novos exercicios

---
+++ Outros/desafio1.py
@@ -0,0 +1,4 @@
+notas = sum([int(input('Digite a primeira nota: ')), int(input('Digite a segunda nota: ')), 
+        int(input('Digite a terceira nota: ')), int(input('Digite a quarta nota: '))])
+media = notas / 4
+print(f'A média aritmética é {media}')
