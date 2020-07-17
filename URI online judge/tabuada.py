a370c3fc4a505216f7214c5601dc52c8f3e4ca41
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-17 17:16:09 -0300

    Delete tabuada.py

--- URI online judge/tabuada.py
+++
@@ -1,8 +0,0 @@
-'''
-    Programa que mostra a tabuada do numero escolhido
-    pelo usuario
-'''
-n = int(input('Informe o numero para saber a sua tabuada:  '))
-for c in range(0, 11):
-    resp = n * c
-    print(f'{n} x {c} = {resp}')
