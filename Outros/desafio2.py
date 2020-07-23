cd618e7c0ec633f2e9027ec877d3ab64c32993ed
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-07-23 11:32:34 -0300

    Update desafio2.py

--- Outros/desafio2.py
+++ Outros/desafio2.py
@@ -1,4 +1,4 @@
-lado = int(input('Digite o valor correspondente ao lado de um quadrado: '))
+lado = float(input('Digite o valor correspondente ao lado de um quadrado: '))
 area = lado ** 2
 perimetro = lado * 4
 print(f'perímetro: {perimetro} - área: {area}')
