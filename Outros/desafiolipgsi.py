a57bbf56df22415075587292b2a1eaf577642f68
José Gabriel <67514416+josefloresgabriel@users.noreply.github.com>
2020-08-01 17:30:30 -0300

    Update desafiolipgsi.py

--- Outros/desafiolipgsi.py
+++ Outros/desafiolipgsi.py
@@ -5,3 +5,6 @@ altura = float(input('Altura: '))
 n_sus = str(input('Número do SUS:'))
 diagnostico = str(input('Diagnostico: '))
 print(f'{nome},{idade},{peso},{altura},{n_sus},{diagnostico}')
+'''954,7 kb
+886,9 kb
+https://repl.it/repls/DeliciousHastyIntegers#main.py'''
